9eab1a6399c8196c199b10e688a226302cce8809
王士远 <wangshiyuan@192.168.2.102>
2023-10-24 08:29:35 +0800

    use weui components

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,26 +1,37 @@
 Page({
-  navigateTo() {
-    // wx.navigateTo({
-    //   url: '../index/index',
-    // })
-    wx.navigateBack();
-    const appBaseInfo = wx.getAppBaseInfo();
-    console.log(appBaseInfo.SDKVersion)
-    console.log(appBaseInfo.enableDebug)
-    console.log(appBaseInfo.host)
-    console.log(appBaseInfo.language)
-    console.log(appBaseInfo.version)
-    console.log(appBaseInfo.theme)
-  },
-  fetchData() {
-    wx.request({
-      url: 'https://dp.zhito.com:39020/api/user/list/all',
-      success(res) {
-        console.log(res);
-      },
-      fail(err) {
-        console.log(err);
-      }
-    })
-  }
+    data: {
+        show: false,
+        buttons: [{
+            text: '取消'
+        }, {
+            text: '确认'
+        }]
+    },
+    navigateTo() {
+        wx.navigateTo({
+            url: '../index/index',
+        })
+        // wx.navigateBack();
+        const appBaseInfo = wx.getAppBaseInfo();
+        console.log(appBaseInfo.SDKVersion)
+        console.log(appBaseInfo.enableDebug)
+        console.log(appBaseInfo.host)
+        console.log(appBaseInfo.language)
+        console.log(appBaseInfo.version)
+        console.log(appBaseInfo.theme)
+    },
+    fetchData() {
+        this.setData({
+            show: true
+        });
+        wx.request({
+            url: 'https://dp.zhito.com:39020/api/user/list/all',
+            success(res) {
+                console.log(res);
+            },
+            fail(err) {
+                console.log(err);
+            }
+        })
+    }
 })
