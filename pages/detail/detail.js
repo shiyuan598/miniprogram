d85877fa6b1c06feeac4b8d02a3be392449a94b2
unknown <wangshiyuan@ZHITO.com>
2023-10-26 10:57:02 +0800

    demo

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,37 +1,38 @@
 Page({
-    data: {
-        show: false,
-        buttons: [{
-            text: '取消'
-        }, {
-            text: '确认'
-        }]
-    },
-    navigateTo() {
-        wx.navigateTo({
-            url: '../index/index',
-        })
-        // wx.navigateBack();
-        const appBaseInfo = wx.getAppBaseInfo();
-        console.log(appBaseInfo.SDKVersion)
-        console.log(appBaseInfo.enableDebug)
-        console.log(appBaseInfo.host)
-        console.log(appBaseInfo.language)
-        console.log(appBaseInfo.version)
-        console.log(appBaseInfo.theme)
-    },
-    fetchData() {
-        this.setData({
-            show: true
-        });
-        wx.request({
-            url: 'https://dp.zhito.com:39020/api/user/list/all',
-            success(res) {
-                console.log(res);
-            },
-            fail(err) {
-                console.log(err);
-            }
-        })
-    }
+  data: {
+    show: false,
+    buttons: [{
+      text: '取消'
+    }, {
+      text: '确认'
+    }],
+  },
+  navigateTo() {
+    wx.switchTab({
+      url: '../index/index',
+    });
+    console.log("hello hello");
+  },
+  fetchData() {
+    this.setData({
+      show: true
+    });
+    wx.request({
+      url: 'https://dp.zhito.com:39020/api/user/list/all',
+      success(res) {
+        console.log(res);
+      },
+      fail(err) {
+        console.log(err);
+      }
+    })
+  },
+  tapDialogButton({
+    detail
+  }) {
+    console.log(detail);
+    this.setData({
+      show: false
+    });
+  }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -7,6 +7,7 @@ Page({
   login() {
     wx.login({
       success: (res) => {
+        console.log(res);
         wx.showToast({
           title: '登录成功',
         })
@@ -53,8 +54,22 @@ Page({
       mediaType: ['image'],
       sourceType: ["album", "camera"],
       success: (res) => {
-        console.log(res.tempFiles);
+        console.log(res.data, res.tempFiles);
         this.setData({image: res.tempFiles[0].tempFilePath})
+        const tempFilePath = res.tempFiles[0].tempFilePath;
+        wx.uploadFile({
+          filePath: tempFilePath,
+          name: 'file',
+          url: 'http://172.16.12.84:9030/upload',
+          success: (res) => {
+            console.log("上传成功");
+            const data = JSON.parse(res.data);
+            console.log(data);
+          },
+          fail: (err) => {
+            console.log("上传失败", err);
+          }
+        })
       }
     })
   }
